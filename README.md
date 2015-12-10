260b6e7ad57a5808ffca6cb49b05176e321cb4cc
Michael Chadbourne <ziglish@gmail.com>
2015-12-10 18:10:14 -0500

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # IAI
-Itemized Artificial Intelligence is a fucking joke of a way to an A.
+Itemizing Artificial Intelligence:
+A Project exploring learning in an inventory allotment scenario.
+Gives an agent the opportunity to learn the best itemization schemes
+
+Authors:
+    Michael Chadbourne
+    Tim Webber
+    Mateo Freye
+
+Last Edited: 12/10/15
+
+Files:
+    items.py:
+    contains the item classes as well as instantiations of the items which make
+    up the item-store's catalog.
+
+    shopping.py:
+    contains the processes used in assigning a personal inventory from the stock
+    available in the item-store. This is where the learning-agent applies it's
+    knowledge.
+
+    gameObjects.py:
+    contains the classes which represent the structure of the testing
+    environment. This is where the learning-agent gathers new information, and
+    Tests the effectiveness of their item build.
+
+    IAI.py:
+    The Main-File for the project: Runs the testing suite and displays results.
+
+DESIGN:
+    items.py contains item classes used by the shop, and in the hunting phase.
+    These classes, while not enforced by python, must be adhered to when
+    creating new items, so as to assure consistency of function.
+
+    shopping.py uses the a <FORMAT DECISION> passed to it, to generate an
+    inventory based upon the items available in <FORMAT DECISION> and the
+    knowledge of items learned from "Journal Entries" received from the hunting
+    phase.
+
+    The JOURNAL ENTRIES are in the format:
+    (String action, int time, int hp, int hits) or
+    ("HIT", int time, int hp, Listof<String> tags)
+
+    Where 'action' is 'MOVE' or the name of an item.
+    'time' is the number of turns since the level began
+    'hp' is the Hero's hp after the reported action occurred
+    'hits' is the number of enemies affected by the action
+    and 'tags' in the "HIT" case is a List of iterm-names where each name in the
+        list is a weapon which could have helped the hero.
+        
--- gameObjects.py
+++ gameObjects.py
@@ -9,24 +9,23 @@ class heroAgent:
     def refine(rawItems):
         #TODO return as a list of game_items
         return []
-    
+
     def __init__(self, start, rawItems):
         self.health   = 100              # health points
         self.position = start            # X, Y position as tuple
         self.invtry   = refine(rawItems) # A list of game_items available to the hero
         self.diary    = []               # A list of the actions taken by the hero
-        
-    
+        # Journal Entries
     ''' advance:
       * Progresses the hero forward acording
       * to a basic set of rules based upon villian position
       * and moving north
     '''
-    def advance(self, vilPosits):
+    def advance(self, gameState):
         #TODO 'A basic set of rules'
         self.position = (self.position[0], self.position[1] + 1)
-    
-    
+
+
 ''' villiansAgent:
   * A basic decision agent for enemies
   * Acts on a list of positions to attack the Hero
@@ -35,21 +34,21 @@ class villiansAgent:
     def _init_(self, positions):
         self.positions = positions
         self.healthi   = [20] * len(positions)
-        
+
     ''' stats:
       * returns a tuple with the stats for the specified villian
       * as (Position, HP)
     '''
     def stats(self, index):
         return (self.positions[i], self.healthi[i])
-        
+
     ''' advance:
       * Progresses each of the locations in 'Positions' forward
       * acording to a basic set of rules based upon hero position
     '''
-    def advance(self, heroPos):
+    def advance(self, gameState):
         #TODO another 'basic set of rules'
-    
+
 ''' gameMap:
   * A representation of a game-level
 '''
@@ -60,7 +59,7 @@ class gameMap:
         self.levelExit = end    # Win-Condition
         self.vilSpawns = spawns # Enemy start locations
 
-        
+
 ''' gameState:
   * A representation of the current state of the game
 '''
@@ -71,30 +70,28 @@ class gameState:
         self.vlns = villiansAgent(map.vilSpawns)
         self.maxVil = len(map.vilSpawns)
         self.time = 0
-        
+
     ''' gameOver:
       * returns true if the game has reached an end-conditions
       * false otherwise
     '''
     def gameOver(self):
         return (self.hero.position == self.map.levelExit or self.hero.health <= 0)
-        
+
     ''' score:
       * returns a score tuple, consisting of a boolean and an integer score-value
       * the boolean will be true if the win-condition is the only end-condition
-      * currently met and false otherwise 
+      * currently met and false otherwise
     '''
     def score(self):
         victory = self.hero.position == self.map.levelExit and not self.hero.health <= 0
         value   = 100 * (self.maxVil - len(self.vlns.positions)) - (5 * self.time) #TODO less arbitrary
         return (victory, value)
-        
+
     ''' advance:
       * Progresses the game state forward by one unit of time.
     '''
     def advance(self):
         if (self.gameOver()): return
-        self.hero.advance(self.vlns.positions)
-        self.vlns.advance(self.hero.position)
-
-    
+        self.hero.advance(self)
+        self.vlns.advance(self)
