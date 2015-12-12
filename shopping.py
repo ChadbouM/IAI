561d4a07bb9401b5291e0d22adaa06226a1130a8
TauVolantis <freyre.m@husky.neu.edu>
2015-12-12 01:01:58 -0500

    Small mod to shopping.py to prepar for work later

--- shopping.py
+++ shopping.py
@@ -1,4 +1,5 @@
 import sys
+from wiseClass import wisdom
 
 ''' shopping.py: IAI Project
   * 
@@ -13,24 +14,18 @@ import sys
 '''
 
 ''' shopping class:
-  * This class stores a diary of moves
-  * and actions taken during the game
-  * and determines what items to include
+  * Determines what items to include
   * in the hero's inventory the next time
-  * he goes hunting son
+  * he goes hunting
 '''
 class shopping
 
 	'''
-	 * A diary item is:
-	 * - (string, int, int, int or list of strings)
-	 * - (item used or move or damage, turn number, hit points, kills or list of tags)
+	 * 
 	'''
-	def __init__(self, diary):
-        self.diary = diary
+	def __init__(self, maxWeight=100):
+        self.maxWeight = maxWeight # maximum inventory space for each hunt
 
-    '''
-    If the first item in the tuple is a "hit", then the last item will be a list of tags
-    '''
+
     def whatToBuy():
     	
