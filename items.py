570be14e2f028ee935b582592b63b97797ac8e12
ZiG Chad <Ziglish@gmail.com>
2015-12-13 15:02:28 -0500

    Invoking the wrath of tim

--- items.py
+++ items.py
@@ -16,9 +16,11 @@
 '''
 class SuperItem(object):
 
-    def __init__(self):
-        self.name = ""
-        self.weight = 0
+    def __init__(self, name=None, weight=0):
+        # Item name as string, used for ID
+        self.name   = name
+        # Representation of space taken by the item
+        self.weight = weight
 
     ''' getName:
       * Returns the name of the object
@@ -32,180 +34,145 @@ class SuperItem(object):
     def getWeight(self):
         return self.weight
 
-''' ======================== NON-ABSTRACT CLASSES ======================== '''
+''' ======================== SEMI-ABSTRACT CLASSES ======================== '''
 ''' Medkit:
-  * An abstract class for medkits
+  * The semi-stract class for medkits
 '''
 class Medkit(SuperItem):
-    def __init__(self):
-        self.name = "medkit"
-        self.healPower = 10
-        self.weight = 5
+    def __init__(self, name='gen_medkit', hp=10, weight=5):
+        SuperItem.__init__(self, name, weight) 
+        self.healPower = hp
+ 
+''' Weapon:
+  * The semi-stract class for weapons
+''' 
+class Weapon(SuperItem):
+    ''' genAOE:
+      * The generic AOE function
+      * Hits the given target
+    '''
+    def genAOE(pos):
+        return [pos]
+    
+    def __init__(self, name='pistol', weight=10, range=5, AOE=genAOE):
+        SuperItem.__init__(self, name, weight)
+        self.range = range
+        self.AOE   = AOE
 
 '''Ammo:
-  * An abstract class for ammo
+  * The semi-stract class for ammo
 '''
 class Ammo(SuperItem):
-    def __init__(self):
-        self.name = "ammo"
-        self.weight = 1
-        # If we need an "ammo type", it must be here as 
-
-'''Grenade Ammo:
-  * An abstract class for Grenade Ammo
-'''
-class GrenadeAmmo(SuperItem):
-    def __init__(self):
-        self.name = "grenade ammo"
-        self.weight = 1
+    def __init__(self, name="ammo", weight=1, type='GENERIC'):
+        SuperItem.__init__(self, name, weight)
+        '''The .name of the item this ammo reloads
+          *  'GENERIC' can reload anything'''
+        self.type   = type 
 
 '''Candy:
-  * An abstract class for candy
+  * The semi-stract class for candy
 '''
 class Candy(SuperItem):
-    def __init__(self):
-        self.name = "candy"
-        self.weight = 5
-        self.points = 50
+    def __init__(self, name='gen_candy', weight=5, points=50):
+        sSuperItem.__init__(self, name, weight)
+        self.points = points
 
-''' ========================= WEAPONS ========================= '''
-'''Pistol:
-  * The Pistol Weapon Class
+''' ========================= WEAPONS & AMMO ========================= '''
+''' ::Generic Ammo::
+  * Ammo which is heavy, but can reload any weapon
 '''
-class Pistol(SuperItem):
-    def __init__(self):
-      	self.name = "pistol"
-      	self.range = 5
-        self.hitcount = 1
-        self.weight = 10
+GenericAmmo = Ammo('Lando\'s one-size-fits-all ballistic', 5)
 
-    ''' AOE:
-      * Given 'pos' a (int, int) tuple representing the position of
-      * impact, Returns a list of all (int, int) tuples,
-      * representing the set of positions hit by the weapon
-      * [ X ]
-    '''
-    def AOE(self, pos):
-        return [pos]
+'''Pistol:
+  * The Pistol Weapon '''
+Pistol = Weapon()
+PistolAmmo = Ammo('pistol ammo', 1, 'pistol')
 
 '''Shotgun:
-  * The Shotgun Weapon Class
-
-class Shotgun(SuperItem):
-    def __init__(self):
-        self.name = "shotgun"
-        self.range = 3
-        self.hitcount = 3
-        self.weight = 10
-
-    '' AOE:
-      * Given 'pos' a (int, int) tuple representing the position of
-      * impact, Returns a list of all (int, int) tuples,
-      * representing the set of positions hit by the weapon
-    ''
-    def AOE(self, posPlayer, listofTargets):
-        hitList = []
-        counter = 0
-        # hit up to number of possible hitcounts
-        for target in listofTargets:
-            # hit the first three targets within range in the list
-            if self.manhattanDistance(posPlayer, target) <= self.range and counter < self.hitcount:
-                hitList.append(target)
-                counter += 1
-        return hitList
-'''
+  * The Shotgun Weapon'''
+def AOE_Shotgun(pos):
+    ''' [       X      ]
+      * [ X X X X X X X]
+      * [       X      ]
+    '''
+    horizontal  = [(pos[0]+i, pos[1]) for i in range(-3, 4)]
+    verticle    = [(pos[0], pos[1]+i) for i in range(-1, 2)]       
+    return set(horizontal + verticle)
+    
+name_Shotgun = 'shotgun'
+Shotgun = Weapon(name_Shotgun, 10, 3, AOE_Shotgun)  
+ShotgunAmmo = Ammo('shell', 1, name_Shotgun)
 
 '''Rifle:
-  * The Rifle Weapon Class
+  * The Rifle Weapon
 '''
-class Rifle(SuperItem):
-    def __init__(self):
-        self.name = "rifle"
-        self.range = 10
-        self.hitcount = 1
-        self.weight = 10
-
-    ''' AOE:
-      * Given 'posPlayer' a (int, int) tuple representing the position of
-      * the player and 'listofTargets' a list of posn tuples representing
-      * the positions of the point of enemies, Returns a list of all enemy
-      * positions (int, int) hit by the weapon. Position of first impact 
-      * is always in the middle of the returned list.
-      * [ X  X  X ]
+def AOE_Rifle(pos):
+    ''' [ X  X  X ]
       * [ X  X  X ]
       * [ X  X  X ]
     '''
-    def AOE(self, pos):
-        return [pos, 
-        (pos[0] - 1, pos[1] + 0),
-        (pos[0] - 1, pos[1] - 1),
-        (pos[0] + 0, pos[1] + 1),
-        (pos[0] + 1, pos[1] + 1),
-        (pos[0] + 1, pos[1] + 0),
-        (pos[0] + 1, pos[1] - 1),
-        (pos[0] + 0, pos[1] - 1),
-        (pos[0] - 1, pos[1] - 1)]
+    lineNorth = [(pos[0]+i, pos[1]+1) for i in range (-1,2)]
+    lineMain  = [(pos[0]+i, pos[1])   for i in range (-1,2)]
+    lineSouth = [(pos[0]+i, pos[1]-1) for i in range (-1,2)]  
+    return list(set(lineNorth + lineMain + lineSouth))
 
-'''GrenadeLauncher:
-  * The Grenade Launcher Weapon Class
+name_Rifle = 'rifle'
+Rifle     = Weapon(name_Rifle, 10, 10, AOE_Rifle)
+RifleAmmo = Ammo('rifle ammo', 1, name_Rifle)
+    
+''' ::GrenadeLauncher::
+  * The Grenade Launcher Weapon and Ammo
 '''
-class GrenadeLauncher(SuperItem):
-    def __init__(self):
-        self.name = "grenade launcher"
-        self.range = 7
-        self.hitcount = 5
-        self.weight = 10
-
-    ''' AOE:
-      * Given 'posPlayer' a (int, int) tuple representing the position of
-      * the player and 'listofTargets' a list of posn tuples representing
-      * the positions of the point of enemies, Returns a list of all enemy
-      * positions (int, int) hit by the weapon. Position of first impact 
-      * is always in the middle of the returned list.
-      * [ X  X  X  X  X ]
+def AOE_Grenade(pos):
+    ''' [ X  X  X  X  X ]
       * [ X  X  X  X  X ]
       * [ X  X  X  X  X ]
       * [ X  X  X  X  X ]
       * [ X  X  X  X  X ]
     '''
-    def AOE(self, pos):
-        return [pos, 
-        (pos[0] - 1, pos[1] + 0),
-        (pos[0] - 1, pos[1] - 1),
-        (pos[0] + 0, pos[1] + 1),
-        (pos[0] + 1, pos[1] + 1),
-        (pos[0] + 1, pos[1] + 0),
-        (pos[0] + 1, pos[1] - 1),
-        (pos[0] + 0, pos[1] - 1),
-        (pos[0] - 1, pos[1] - 1),  # Here and above same as rifle
-        (pos[0] - 2, pos[1] + 0),
-        (pos[0] - 2, pos[1] + 1),
-        (pos[0] - 2, pos[1] + 2),
-        (pos[0] - 1, pos[1] + 2),
-        (pos[0] - 0, pos[1] + 2),
-        (pos[0] + 1, pos[1] + 2),
-        (pos[0] + 2, pos[1] + 2),
-        (pos[0] + 2, pos[1] + 1),
-        (pos[0] + 2, pos[1] + 0),
-        (pos[0] + 2, pos[1] - 1),
-        (pos[0] + 2, pos[1] - 2),
-        (pos[0] + 1, pos[1] - 2),
-        (pos[0] + 0, pos[1] - 2),
-        (pos[0] - 1, pos[1] - 2),
-        (pos[0] - 2, pos[1] - 2),
-        (pos[0] - 2, pos[1] - 1)]
+    rtrn = []
+    for j in range(-2, 3):
+        rtrn += [(pos[0]+i, pos[1]+j) for i in range(-2,3)]
+    return rtrn
+        
+name_Grenade    = 'grenade launcher'       
+GrenadeLauncher = Weapon(name_Grenade, 10, 7, AOE_Grenade)
+GrenadeAmmo     = Ammo('grenade', 2, name_Grenade) 
+
+    
 
 #Testing
-target = (0,0)
+if __name__ == '__main__' and __debug__:
+    def printHits(hitList):
+        boundX = [0,0]
+        boundY = [0,0]
+        for x, y in hitList:
+            if x < boundX[0]: boundX[0]=x
+            if x > boundX[1]: boundX[1]=x
+            if y < boundY[0]: boundY[0]=y
+            if y > boundY[1]: boundY[1]=y
+        for i in range(boundY[0], boundY[1]+1):
+            pline = ""
+            for j in range(boundX[0], boundX[1]+1):
+                if (j, i) in hitList: pline+="[X]"
+                else:                 pline+="   "
+            print "\t" + pline
+        print " "
+            
+    print " * RUNNING TESTS: items.py *"
 
-a = Pistol()
-b = Rifle()
-c = GrenadeLauncher()
-hitenemiesPistol = a.AOE(target)
-hitenemiesRifle = b.AOE(target)
-hitenemiesGrenadeLauncher = c.AOE(target)
+    target = (0,0)
 
-print "\n", "Pistol hit: ", hitenemiesPistol, "\n"
-print "Rifle hit: ", hitenemiesRifle, "\n"
-print "Grenade Launcher hit: ", hitenemiesGrenadeLauncher, "\n"
-print "DataType of elements: ", type(hitenemiesGrenadeLauncher[0])
+    hitenemiesPistol          = Pistol.AOE(target)
+    hitenemiesShotgun         = Shotgun.AOE(target)
+    hitenemiesRifle           = Rifle.AOE(target)
+    hitenemiesGrenadeLauncher = GrenadeLauncher.AOE(target)
+    
+    print "\nPistol Hits:"
+    printHits(hitenemiesPistol)
+    print "Shotgun Hits:"
+    printHits(hitenemiesShotgun)
+    print "Rifle Hits:"
+    printHits(hitenemiesRifle)
+    print "Grenade Launcher Hits:"
+    printHits(hitenemiesGrenadeLauncher)
